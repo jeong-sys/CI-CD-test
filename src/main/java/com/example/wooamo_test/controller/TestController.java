1d071f4294db8bc1c4dd83d197143472c8cf2b92
you <20211939@edu.hanbat.ac.kr>
2025-02-19 02:00:33 +0900

    test: ci 실행

--- src/main/java/com/example/wooamo_test/controller/TestController.java
+++ src/main/java/com/example/wooamo_test/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        System.out.println("test2");
+        System.out.println("test3");
         return ResponseEntity.ok("Hello World test !!!!!");
     }
 }
