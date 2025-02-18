cb01d844a4b90dc442cd39fc97d5899c3ffe8e8e
you <20211939@edu.hanbat.ac.kr>
2025-02-18 22:54:06 +0900

    feat: ignore 수정 - .env 생성

---
+++ src/test/java/com/example/wooamo_test/WooamoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wooamo_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooamoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
