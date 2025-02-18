d546f478238553578079d284c1ff2d0c2195989f
you <20211939@edu.hanbat.ac.kr>
2025-02-18 23:38:49 +0900

    feat: CI 테스트를 위한 testCode 작성

--- src/main/java/com/example/wooamo_test/controller/TestController.java
+++ src/main/java/com/example/wooamo_test/controller/TestController.java
@@ -1,14 +1,34 @@
 package com.example.wooamo_test.controller;
 
+import com.example.wooamo_test.entity.User;
+import com.example.wooamo_test.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class TestController {
+    private final UserService userService;
+
     @GetMapping("/test")
     public ResponseEntity<String> test() {
         System.out.println("test");
         return ResponseEntity.ok("Hello World test !!!!!");
     }
+
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.save(user));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        Optional<User> user = userService.findByUserEmail(email);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
