99626fd78caaf3173931548e72bc1d64d7469427
you <20211939@edu.hanbat.ac.kr>
2025-02-19 16:32:35 +0900

    feat: classpath:.env 추가

--- src/main/java/com/example/wooamo_test/WooamoTestApplication.java
+++ src/main/java/com/example/wooamo_test/WooamoTestApplication.java
@@ -2,8 +2,10 @@ package com.example.wooamo_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:.env")
 public class WooamoTestApplication {
 
     public static void main(String[] args) {
