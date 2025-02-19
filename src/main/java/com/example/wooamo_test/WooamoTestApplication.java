367f612cea7e4336edd78886497faec5f3724d3a
you <20211939@edu.hanbat.ac.kr>
2025-02-19 16:39:04 +0900

    feat: .env 임의 추가 test

--- src/main/java/com/example/wooamo_test/WooamoTestApplication.java
+++ src/main/java/com/example/wooamo_test/WooamoTestApplication.java
@@ -3,7 +3,9 @@ package com.example.wooamo_test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @PropertySource("classpath:.env")
 public class WooamoTestApplication {
